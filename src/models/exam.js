e19839ecf24883320d2230eadbc95e40a884d663
PRIYANSHU NAYAK <155828894+CODERAI24@users.noreply.github.com>
2026-01-13 15:04:18 +0530

    Add Exam model

---
+++ src/models/exam.js
@@ -0,0 +1,18 @@
+export const Exam = {
+  id: "",
+  name: "",
+  subjects: [
+    {
+      subjectId: "",
+      chapters: [
+        {
+          chapterId: "",
+          theoryProgress: 0,
+          practiceProgress: 0,
+          weightage: 0
+        }
+      ]
+    }
+  ],
+  archived: false
+};
