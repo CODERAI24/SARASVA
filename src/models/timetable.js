f9cc3d78ae62cf4c538e0d8172aa267a34949178
PRIYANSHU NAYAK <155828894+CODERAI24@users.noreply.github.com>
2026-01-13 15:01:01 +0530

    Add Timetable model

---
+++ src/models/timetable.js
@@ -0,0 +1,15 @@
+export const Timetable = {
+  id: "",
+  name: "",    
+  active: false,
+  createdAt: null,
+  archived: false,
+  slots: [
+    {
+      day: "Monday",
+      subjectId: "",
+      startTime: "09:00",
+      endTime: "10:00"
+    }
+  ]
+};
