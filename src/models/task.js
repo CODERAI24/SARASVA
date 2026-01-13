78f70ff51a5293121e698404cb83a0353c6f68ab
PRIYANSHU NAYAK <155828894+CODERAI24@users.noreply.github.com>
2026-01-13 14:59:53 +0530

    Add Task model

---
+++ src/models/task.js
@@ -0,0 +1,11 @@
+export const Task = {
+  id: "",
+  title: "",
+  description: "",
+  dueDate: null,
+  priority: "medium", // low | medium | high
+  completed: false,
+  createdAt: null,
+  updatedAt: null,
+  archived: false
+};
